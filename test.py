8c3e95f35a067a7eefd3ef68fe95f84b8622e408
Sarang Gopal Menewar <sarangmenewar100@gmail.com>
2026-02-06 20:33:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import streamlit as st
-import pandas as pd
 
-st.title("Dataset Checker")
-
-st.write("App started successfully ✅")
-
-file = st.file_uploader("Upload CSV", type=["csv"])
-
-if file:
-    st.write("File uploaded ✅")
-    df = pd.read_csv(file)
-    st.dataframe(df.head())
-else:
-    st.warning("Please upload a CSV file")
